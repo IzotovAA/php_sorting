33ea0577125bef0c3eb2cd8a653a848428b92604
IzotovAA <izotovandreiy@gmail.com>
2024-11-05 14:48:58 +0300

    update index.php

--- app/index.php
+++ app/index.php
@@ -9,10 +9,10 @@ set_time_limit(60);
 $test = new \Classes\Testing();
 
 // $testName variants: ['ALL', 'BUBLE', 'SELECTION', 'QUICK', 'MERGE']
-$test->startTestSorting(1000, 'ALL', false);
+// $test->startTestSorting(1000, 'ALL', false);
 
 // $testName variants: ['ALL', 'LINEAR', 'BINARY']
 // $test->startTestSearching([1,2,3,4,5,6,7,8,9,10,11,12], 9, 'ALL', true);
-// $test->startTestSearching(50, 5, 'ALL', true);
+$test->startTestSearching(50, 5, 'ALL', true);
 
 
