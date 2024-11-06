4aedb7bffecd775067248db880ac9d8b94065956
IzotovAA <izotovandreiy@gmail.com>
2024-11-06 12:05:47 +0300

    code refactoring

--- app/index.php
+++ app/index.php
@@ -6,15 +6,7 @@ require_once 'vendor/autoload.php';
 
 set_time_limit(60);
 
-$reverse = new \Classes\StringReverse();
+$test = new \Classes\Testing();
 
-echo 'Привет Hello, world!<br>';
-echo $reverse->reverse('Привет Hello, world!') . '<br><br>';
-
-echo 'Пустая строка<br>';
-echo $reverse->reverse('') . '<br><br>';
-
-echo 'Лёша на полке клопа нашёл<br>';
-echo $reverse->reverse('Лёша на полке клопа нашёл') . '<br><br>';
-
-
+// $testName variants: ['ALL', 'BUBLE', 'SELECTION', 'QUICK', 'MERGE']
+$test->startTestSorting(1000, 'ALL', false);
